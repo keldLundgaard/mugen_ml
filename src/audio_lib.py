e85e6c91e32419f0f6196608432d8c7ec1bc71c4
keldlundgaard <keld.lundgaard@gmail.com>
2023-02-18 17:42:06 -0500

    updated audiolib to use float32

--- src/audio_lib.py
+++ src/audio_lib.py
@@ -1,28 +1,30 @@
-from miniaudio import mp3_read_s16
-import numpy
+from miniaudio import mp3_read_f32
+from IPython.display import Audio
+import numpy as np
 
 class Mp3Stream:
-	"""
-	Small simple python class to open, load, and play mp3 files.
-	"""
+    """
+    Small simple python class to open, load, and play mp3 files.
+    """
     
     def __init__(self, fp):
         self.fp = fp
         self.decoded = None
         self.rate = None
         
-    def np_decode(self, n_size, offset):
+    def np_decode(self, n_size, offset=0):
         decoded = self.decode(n_size, offset)
-        return np.array(decoded.samples, dtype=numpy.int16).reshape((-1, decoded.nchannels))
+        return np.array(decoded.samples, dtype=np.float32).reshape((-1, decoded.nchannels))
         
-    def decode(self, n_size, offset):
+    def decode(self, n_size, offset=0):
+        assert n_size > 0
+        n_size = int(n_size)
         with open(self.fp, "rb") as f:
             f.seek(offset, 0)
-            decoded = mp3_read_s16(f.read(n_size))
+            decoded = mp3_read_f32(f.read(n_size))
         self.decoded = decoded
         self.sample_rate = decoded.sample_rate
         return decoded
 
-    def play(self, n_size=-1, offset=0, autoplay=False):
-        npa = self.np_decode(n_size, offset)
-        return Audio([npa[:, 0], npa[:, 1]],rate=self.sample_rate, autoplay=autoplay)
+    def play(self, autoplay=False):
+        return Audio(self.fp, autoplay=autoplay)
