2729033997eaf9939eb056e664b928358e01cf63
keldlundgaard <keld.lundgaard@gmail.com>
2023-03-28 23:07:21 -0400

    adding pytorch lightning

---
+++ src/genre_lightning.py
@@ -0,0 +1,70 @@
+import os, sys, time, pickle
+from pathlib import Path
+from collections import Counter
+from IPython.display import Audio
+
+PROJECT_DIR = Path(sys.path[0])/".."
+DATA_DIR = PROJECT_DIR/"data"
+SRC_DIR = PROJECT_DIR/"src"
+DEPS_DIR = PROJECT_DIR/"deps"
+
+sys.path.append(str(SRC_DIR))
+sys.path.append(str(DEPS_DIR))
+
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from fairseq_wav2vec import Wav2Vec2Config, Wav2Vec2Model
+import pandas as pd
+
+import lightning.pytorch as pl
+
+from mugen_train import (
+    musicDataset, IDX_to_GENRE, GENRE_TO_IDX, 
+    get_music_data_loaders, GenreClassifier, 
+#     train_genre_predict, validation_genre_predict,
+    GenreEncoder,
+    get_num_params)
+
+
+if __name__ == "__main__":
+    feature_size = 128
+    n_genres = 50
+    top50_genre_samples_df = pd.read_csv("/n1Tb/sc_mp3_top50_genre_samples.tsv_gz", compression='gzip', sep='\t')
+
+    train_loader, valid_loader = get_music_data_loaders(top50_genre_samples_df, cut=8e4)
+
+    # precision = 16
+
+    encoder = Wav2Vec2Model(Wav2Vec2Config)
+
+    t_specs = {
+    #     "d_model": 128, 
+        "dim_feedforward": 64, 
+        "num_decoder_layers": 0,
+        "num_encoder_layers": 0,
+        "nhead": 1,
+    }
+    decoder = GenreClassifier(n_genres, feature_size, t_specs=t_specs, use_transformer=False)
+
+    print(f"wav2vec-model n_params: {get_num_params(encoder):,d}")
+    print(f"genre_classifier n_params: {get_num_params(decoder):,d}")
+
+    genre_predictor = GenreEncoder(encoder, decoder)
+
+    # torch.multiprocessing.set_start_method('spawn')
+
+    trainer = pl.Trainer(
+        limit_train_batches=1000, 
+        max_epochs=1, 
+        accelerator="gpu", 
+        devices=2,
+        strategy='ddp',     
+        # strategy='ddp_notebook',     
+    #     precision='16-mixed'
+    #     precision=16, 
+    #     devices=2, 
+    )
+
+    trainer.fit(model=genre_predictor, train_dataloaders=train_loader)
--- src/mugen_train.py
+++ src/mugen_train.py
@@ -1,111 +1,106 @@
+import os
+
+from tqdm import tqdm
+
 import numpy as np
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-
+import lightning.pytorch as pl
 
 from audio_lib import Mp3Stream
 
-def get_batch_genre_one_hot(genres, n_genres, device):
-        return (
-            torch.nn.functional.one_hot(torch.tensor(genres), n_genres)
-            .requires_grad_(False)
-            .type(torch.float)
-            .to(device)
-        )
+N_CPU_CORES = os.cpu_count()
 
+def get_lr(trainer, model, train_loader, plot=False):
+    tuner = pl.tuner.tuning.Tuner(trainer)
+    out_lr_finder = tuner.lr_find(model=model, train_dataloaders=train_loader)
 
-class MusicCNN(nn.Module):
-    def __init__(self, n_classes):
-        super(MusicCNN, self).__init__()
+    lrf_lr = out_lr_finder.results["lr"]
+    lrf_loss = out_lr_finder.results["loss"]
+    opt_idx = out_lr_finder._optimal_idx
+    if plot:
+        plt.plot(lrf_lr, lrf_loss);
+        plt.plot(lrf_lr[opt_idx], lrf_loss[opt_idx], "x", label="optimum");
+        plt.legend()
+
+        plt.xscale("log");
+    print(f"Optimal lr: {lrf_lr[opt_idx]}")        
+    return lrf_lr[opt_idx]
+
+class GenreEncoder(pl.LightningModule):
+    def __init__(self, encoder, decoder, lr=1e-3):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.BCELoss = torch.nn.BCELoss()
+        self.lr = lr
+
+    def training_step(self, batch, batch_idx):
+        lpcms, genres = batch
+        outputs = self.encoder(lpcms)
+        z = outputs["x"]
+        z, genres = remove_inf_rows(z, genres)
         
-        # Define convolutional layers
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        
-        # Define pooling layer
-        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
-        
-        # Define fully connected layers
-        self.fc1 = nn.Linear(in_features=128*4*4, out_features=256)
-        self.fc2 = nn.Linear(in_features=256, out_features=n_classes)
-        
-    def forward(self, x):
-        # Perform convolutional layers and pooling
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        x = F.relu(self.conv3(x))
-        x = self.pool(x)
-        x = F.relu(self.conv4(x))
-        x = self.pool(x)
-        
-        # Flatten output for fully connected layers
-        x = x.view(-1, 128*4*4)
-        
-        # Perform fully connected layers
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        
-        return x
+        genre_props = self.decoder(z)
+
+        # genre prediction loss
+        loss = self.BCELoss(genre_props, genres)
+
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.decoder.parameters(), lr=self.lr)
+        return optimizer
 
 class GenreClassifier(nn.Module):
     def __init__(self, n_classes, feature_size, t_specs={}, use_transformer=True):
         super().__init__()
         self.feature_size = feature_size
         self.use_transformer = use_transformer
+        self.encoder_out_size = 101
 
         if self.use_transformer:
             self.genre_transformer = nn.Transformer(
                 **{**t_specs, **{"d_model": feature_size}}
             )
 
-        self.fc = nn.Linear(feature_size, n_classes)
+            self.fc = nn.Linear(feature_size, n_classes)
+        else:
+            self.fc = nn.Linear(self.encoder_out_size, n_classes)
 
     def forward(self, x):
-        # Add padding so that it works with transformer 
-        zero_padding = torch.zeros((self.feature_size - x.shape[0], *x.shape[1:])).to("cuda")
-        x = torch.cat((x, zero_padding), dim=0)
-
         # shape is now batch, timesteps, layer features
         x = x.permute(1, 2, 0)
-
+        
         if self.use_transformer:
+            # Add padding so that it works with transformer 
+            # adding padding to get to feature size 
+            x = torch.pad(x, (0, self.feature_size-101))
             x = self.genre_transformer(x, x)
         x = x.mean(dim=1)
         prob = torch.sigmoid(self.fc(x))
         return prob
 
+def remove_inf_rows(x, genres, debug=False):
+    # discard where there is inf values 
+    num_inf = torch.sum(torch.isinf(x))
+    if num_inf:
+        ignore_samples = torch.where(x.isinf())[1].unique()
+        filter_samples = [i not in ignore_samples for i in range(x.shape[1])]
+        x = x[:, filter_samples, :]
+        genres = genres[filter_samples, :]
+        # [genre for i, genre in enumerate(genres) if i not in ignore_samples]
+        if debug:
+            print(f"num_inf in output of model {num_inf}")        
+            print(f"Num of ignored samples: {len(ignore_samples)}") 
+    return x, genres
 
-def train(model, optimizer, data_loader):
-    model.train()
-    running_loss = 0.0
-    for i, (batch, genres) in tqdm(enumerate(data_loader, 0), total=len(data_loader)):
-        optimizer.zero_grad()
-        outputs = model(batch)
-        loss = outputs["features_pen"]
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.to("cpu").item()
-    return running_loss / len(data_loader)
-
-
-def validation(model, data_loader):
-    model.eval()
-    running_loss = 0.0
-    for i, (batch, genres) in tqdm(enumerate(data_loader, 0), total=len(data_loader)):
-        outputs = model(batch)
-        loss = outputs["features_pen"]
-        running_loss += loss.to("cpu").item()
-    return running_loss / len(data_loader)
-
-
-def get_music_data_loaders(music_df, batch_size=12, n_size=1e5, cut=100000, random_mix_stereo=True):
+def get_music_data_loaders(
+        music_df, batch_size=12, n_size=1e5, cut=100000, random_mix_stereo=True):
     train_ds = musicDataset(
         music_df, n_size=n_size, cut=cut, random_mix_stereo=random_mix_stereo)
     valid_ds = musicDataset(
@@ -116,12 +111,14 @@ def get_music_data_loaders(music_df, batch_size=12, n_size=1e5, cut=100000, rand
         batch_size=batch_size,
         shuffle=True,
         collate_fn=music_collate_fn_wrap(cut),
+        num_workers=N_CPU_CORES,
     )
     valid_loader = DataLoader(
         valid_ds,
         batch_size=batch_size,
         shuffle=False,
         collate_fn=music_collate_fn_wrap(cut),
+        num_workers=N_CPU_CORES,
     )
     return train_loader, valid_loader
 
@@ -143,10 +140,9 @@ def music_collate_fn_wrap(cut):
                     genres.append(genre)
                     genres.append(genre)
 
-        return torch.concat(features), genres
+        return torch.concat(features), torch.concat(genres)
 
     return music_collate_fn
-
 
 class musicDataset(Dataset):
     def __init__(
@@ -154,18 +150,18 @@ class musicDataset(Dataset):
         data_df,
         n_size=1e5,
         cut=100000,
-        device="cuda",
         offset=None,
         start_cut=1000,
         random_mix_stereo=False,
+        n_genres=50,
     ):
-        self.device = device
         self.data_df = data_df
         self.n_size = n_size
         self.cut = int(cut)
         self.start_cut = int(start_cut)
         self.offset = offset
         self.random_mix_stereo = random_mix_stereo
+        self.n_genres = n_genres
 
     def __len__(self):
         return len(self.data_df)
@@ -173,23 +169,35 @@ class musicDataset(Dataset):
     def __getitem__(self, idx):
         song_info = self.data_df.iloc[idx]
         genre_idx = GENRE_TO_IDX[song_info["genre"]]
+
+        genre_one_hot = (
+            torch.nn.functional.one_hot(torch.tensor([genre_idx]), self.n_genres)
+            .requires_grad_(False)
+            .type(torch.float)
+        )
+        
         song = Mp3Stream(song_info["nvme_fp"])
         if self.offset is None:
             d = song.np_decode_rand_offset(self.n_size)
         else:
             d = song.np_decode(self.n_size, offset=int(self.offset))
-        if d is not None:
+
+        if d is not None and d.shape[1] not in {1, 2}:
+            print(d.shape)
+            raise ValueError("More than 2 channels! Need a way to handle.")            
+
+        if d is not None and d.shape[1] == 2:
             # Remove a bit of the beginning as the first 250 samples are always around zero
             # start_cut should be >250
             d = torch.tensor(d).movedim(0, -1)[
                 :, self.start_cut : self.cut + self.start_cut
             ]
-            if self.random_mix_stereo:
+            
+            if self.random_mix_stereo and d.shape[0] == 2:
                 LR_mix_ratio = np.random.random()
                 d = LR_mix_ratio * d[0] + (1 - LR_mix_ratio) * d[1]
-            d = d.to(self.device)
-        return d, genre_idx
 
+        return d, genre_one_hot 
 
 def get_num_params(model):
     return np.sum(
@@ -198,8 +206,6 @@ def get_num_params(model):
                 torch.prod(torch.tensor(param.size())).item() 
                 for name, param in model.named_parameters()), 
             dtype=int))
-
-
 
 GENRE_TO_IDX = {
     "edm": 0,
